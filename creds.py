32afa94c0461dba256b187960f1de0f7ca7bd17c
fufufanatic <taiwo.salau@gmail.com>
2020-08-29 23:17:35 +0200

    updated with real creds

--- creds.py
+++ creds.py
@@ -1,10 +1,10 @@
 class Creds:
 
-    tmdb_key = 'your tmdb api key'
-    omdb_key = 'your omdb api key'
-    sg_key = 'your sendgrid api key'
-    my_email = 'your email'
-    target_emails = 'comma separated list of recipient emails'
+    tmdb_key = 'cfd9f23c9dc011d3dc8070d22c2a2a73'
+    omdb_key = '919628fb'
+    sg_key = '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'
+    my_email = 'taiwo.salau@gmail.com'
+    target_emails = 'taiwo.salau@gmail.com, kehinde.salau@gmail.com, chris@cbsaunders.com'
 
     def __init__(self):
         pass
